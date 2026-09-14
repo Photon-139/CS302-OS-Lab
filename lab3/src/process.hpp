84ba9cd1f8e3ea7c85166c23f1856586f0272ae1
Anshuman Rahul <anshumanrahul06@gmail.com>
2026-09-14 15:15:46 +0530

    I do not know what I'm doing

--- lab3/src/process.hpp
+++ lab3/src/process.hpp
@@ -7,7 +7,17 @@ struct Process{
     int arrival_time;
     std::vector<int> cpu_bursts;
     std::vector<int> io_bursts;
-    int remaining_time;
-    int completion_time = -1;
+
+    int io_burst_index = -1;
+    int cpu_burst_index = -1;
+    int cpu_burst_remaining;
+
+    int epoch = 0;
+    int quantum_remaining = 0;
+    int cpu_start_time = -1;
+
+    int current_queue = 0;
+    
     int first_service_time = -1;
+    int completion_time = -1;
 };
--- lab3/src/schedule.cpp
+++ lab3/src/schedule.cpp
@@ -2,7 +2,42 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <algorithm>
+#include <queue>
 #include "process.hpp"
+
+enum class EventType{ARRIVAL=0, IO_COMPLETION=1, CPU_RELEASE=2, PRIORITY_BOOST=3};
+
+struct Event{
+    int pid;
+    int time;
+    int epoch;
+    int cpu_id;
+    EventType event;
+
+    bool operator<(const Event& other) const {
+        if(other.time!=time){
+            return time > other.time;
+        }
+        return event > other.event;
+    }
+};
+
+struct ScheduleEntry{
+    int cpu_id;
+    int pid;
+    int cpu_burst;
+    int start_time;
+    int end_time;
+};
+
+class SchedulingPolicy{
+public:
+    virtual void ready_push(int pid) = 0;
+    virtual void requeue(int pid) = 0;
+
+
+};
 
 std::vector<Process> parse_workload(const char* file_path){
     std::ifstream inputFile(file_path);
@@ -32,6 +67,15 @@ std::vector<Process> parse_workload(const char* file_path){
             }
             cpu_burst = !cpu_burst;
         }
+        if(p.cpu_bursts.empty()){
+            std::cerr << "No burst timings provided, invalid process" << std::endl;
+            exit(1);
+        }
+        p.cpu_burst_remaining = p.cpu_bursts[0];
+        p.cpu_burst_index = 0;
+        if(!p.io_bursts.empty()){
+            p.io_burst_index = 0;
+        }
         processes.push_back(std::move(p));
     }
     inputFile.close();
@@ -51,6 +95,9 @@ int main(int argc, char* argv[]){
     std::cout << "Workload path: " << file_path << std::endl;
 
     std::vector<Process> processes = parse_workload(file_path);
+    std::sort(processes.begin(), processes.end(), [](const Process& p1, const Process& p2){
+        return (p1.arrival_time < p2.arrival_time) || (p1.arrival_time==p2.arrival_time && p1.pid < p2.pid); 
+    });
 
 
     return 0;
